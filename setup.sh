3b93dbea16731ab48d48f7978b99921d6a70e8c4
baselomar <125217474+baselomar@users.noreply.github.com>
2023-05-14 03:44:10 -0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -11,15 +11,7 @@ UnzipFiles(){
 smail_banner() {
 printf "\n"
 printf "\n"
-printf "${red}
-
-╔═╗╔═╗╔═╗╦╔═╔═╗  ╦ ╦╔═╗╔╗\n"
-
-printf "${red}╠╣ ╠═╣║  ╠╩╗║╣   ║║║║╣ ╠╩╗\n"
-
-printf "${red}╚  ╩ ╩╚═╝╩ ╩╚═╝  ╚╩╝╚═╝╚═╝\n"
-
-\n"
+printf "${red}█░░ █▀█ █▀█ █▀▄ █▀█ █░█ █ █▀ █░█\n"
 printf "${red}█▄▄ █▄█ █▀▄ █▄▀ █▀▀ █▀█ █ ▄█ █▀█\n"
 printf "      \e[1;97m  .:.  Version 2.0 Beta  .:.  \e[0m\n"
 printf "\n"
